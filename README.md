616aeae967a5f547b0f643b2ead0fbbe168bcf3b
Ian Scott <chesnekov@gmail.com>
2020-07-16 14:27:26 -0700

    adds front-end logic to display word score count after accepting user input. Does not account for non-letter character inputs yet!
    Co-authored-by: James Henager <jameschenager@gmail.com>

--- README.md
+++ README.md
@@ -15,7 +15,6 @@ _This program will return a Scrabble score based upon the letters present in the
 |The program contains a Dictionary which has every letter and their corresponding score value |N/A | N/A |
 |The ScoreCounter class has a Score() method to take in a string and return a number | H | 4 |
 |The Score() method locates a letter in the dictionary and returns it's value|Letter: "P" |Value: 3  |
-
 |The Score() method should be able to locate the values of multiple letters in a multiple character string and return their cumulative value|String: "Hello" |Value: 8|
 
 |The program is able to accept user input of only letter characters in the console|  |  |
--- ScrabbleScore/Program.cs
+++ ScrabbleScore/Program.cs
@@ -7,7 +7,50 @@ namespace ScrabbleScore
   {
     public static void Main()
     {
+      Console.WriteLine("");
+      Console.WriteLine("Welcome to the Scrabble word value calculator!");
+      Console.WriteLine("Enter a word and I will tell you how many points it is worth: ");
+      Start();
+    }
+    public static void Start()
+    {
+      Console.WriteLine("Which word would you like to know the value of?");
+      string inputWord = Console.ReadLine();
+      int wordResult = ScoreCounter.Score(inputWord.ToUpper());
+      Console.WriteLine("The word " + inputWord + " is worth: " + wordResult + " points!");
+      Start();
+    }
 
+    public static string GetInput()
+    {
+      Console.WriteLine("Which word would you like to know the value of?");
+      string inputWord = Console.ReadLine();
+      letterChecker();
+      if (letterChecker == true)
+      {
+        int wordResult = ScoreCounter.Score(inputWord.ToUpper());
+        Console.WriteLine("The word " + inputWord + " is worth: " + wordResult + " points!");
+      }
+      else
+      {
+        Console.WriteLine("Please make sure your word contains no spaces, numbers or special characters")
+        GetInput();
+      }
+      }
+
+    public static string letterChecker()
+    { foreach(char letter in inputWord)
+      {
+        if (char.IsLetter(letter) != true)
+        {
+          return false;
+        }
+        else
+        {
+          return true;
+        }
+        }
+      
     }
   }
 }
