18a407a4cde942d5dc192e75c533d2910c51ae85
Ian Scott <chesnekov@gmail.com>
2020-07-16 11:07:58 -0700

    adds successful first test for instantiating a Scoure Counter class
    Co-authored-by: James Henager <jameschenager@gmail.com>

--- ScrabbleScore.Tests/ModelTests/ScoreCounter.Tests.cs
+++ ScrabbleScore.Tests/ModelTests/ScoreCounter.Tests.cs
@@ -1,11 +1,16 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ScrabbleScore;
+using ScrabbleScore.Models;
 
 namespace ScrabbleScore.TestTools
 {
   [TestClass]
   public class ScrabbleScoreTests
   {
-    
+    [TestMethod]
+    public void ScoreCounterConstructor_CreatesInstanceOfScoreCounter_ScoreCounter()
+    {
+      ScoreCounter counter = new ScoreCounter();
+      Assert.AreEqual(typeof(ScoreCounter), counter.GetType());
+    }
   }
 }
